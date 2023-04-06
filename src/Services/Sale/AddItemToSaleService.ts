6a199b8bab8dcd01bd0d471de59be11ec2bc9cf3
EduardoMoia22 <eduardomoiasanto@gmail.com>
2023-04-06 16:08:59 -0300

    [+] Mudanças no módulo de vendas

--- src/Services/Sale/AddItemToSaleService.ts
+++ src/Services/Sale/AddItemToSaleService.ts
@@ -1,44 +1,90 @@
+import { Item } from "../../Models/ItemModel";
 import { IItemRepository } from "../../Repositories/ItemRepository";
 import { IProductRepository } from "../../Repositories/ProductRepository";
 import { IStockRepository } from "../../Repositories/StockRepository";
 
 type AddItemToSaleProps = {
-    amount: number
+  amount: number;
+  productID: number;
+  saleID: string;
+};
+
+export class AddItemToSaleService {
+  private readonly itemRepository: IItemRepository;
+  private readonly stockRepository: IStockRepository;
+  private readonly productRepository: IProductRepository;
+
+  constructor(
+    itemRepository: IItemRepository,
+    stockRepository: IStockRepository,
+    productRepository: IProductRepository
+  ) {
+    this.itemRepository = itemRepository;
+    this.stockRepository = stockRepository;
+    this.productRepository = productRepository;
+  }
+
+  async execute({ amount, productID, saleID }: AddItemToSaleProps) {
+    const { id, name, stockID } = await this.productRepository.FindById(
+      productID
+    );
+
+    const stock = await this.stockRepository.FindById(stockID);
+
+    const saleItems = await this.itemRepository.ListAllFromSaleID(saleID);
+
+    if (saleItems) {
+      await this.ChecksThatTheQuantityOfTheProductOnSaleDoesNotExceedTheMinimumStock(
+        saleItems,
+        id,
+        amount,
+        stock.stock,
+        stock.stockMin
+      );
+    }
+
+    if (stock.stock - amount < stock.stockMin) {
+      throw new Error(
+        `A quantidade que está sendo vendida ultrapassa o valor minímo do produto em estoque. ID: ${id} - NOME: ${name}`
+      );
+    }
+    const addItemOnSale = await this.itemRepository.Add({
+      amount,
+      productID,
+      saleID,
+    });
+
+    return addItemOnSale;
+  }
+
+  private async SumQuantityOfSalesProduct(
+    saleItems: Item[],
     productID: number
-    saleID: string
-}
-
-export class AddItemToSaleService{
-    private readonly itemRepository: IItemRepository
-    private readonly stockRepository: IStockRepository
-    private readonly productRepository: IProductRepository
-
-    constructor(
-        itemRepository: IItemRepository,
-        stockRepository: IStockRepository,
-        productRepository: IProductRepository){
-        this.itemRepository = itemRepository
-        this.stockRepository = stockRepository
-        this.productRepository = productRepository
+  ) {
+    let sum = 0;
+    for (let i = 0; i < saleItems.length; i++) {
+      if (saleItems[i].productID === productID) {
+        sum += saleItems[i].amount;
+      }
     }
+    return sum;
+  }
 
-    async execute({amount, productID, saleID}: AddItemToSaleProps){
-        const {id, name, stockID} = await this.productRepository.FindById(productID)
-
-        if(!id){
-            throw new Error("Produto não cadastrado")
-        }
-
-        const stock = await this.stockRepository.FindById(stockID)
-
-        if((stock.stock - amount) < stock.stockMin){
-            throw new Error(`A quantidade que está sendo vendida ultrapassa o valor minímo do produto em estoque. ID: ${id} - NOME: ${name}`)
-        }
-
-        return await this.itemRepository.Add({
-            amount,
-            productID,
-            saleID
-        })
+  private async ChecksThatTheQuantityOfTheProductOnSaleDoesNotExceedTheMinimumStock(
+    saleItems: Item[],
+    id: number,
+    amount: number,
+    stock: number,
+    stockMin: number
+  ) {
+    let sum = await this.SumQuantityOfSalesProduct(saleItems, id);
+    if (sum + amount != amount) {
+      const newAmount = amount + sum;
+      if (stock - newAmount < stockMin) {
+        throw new Error(
+          `Não foi possível adicionar esse produto, pois a quantidade total adicionada ultrapassa o valor minimo de estoque`
+        );
+      }
     }
+  }
 }
